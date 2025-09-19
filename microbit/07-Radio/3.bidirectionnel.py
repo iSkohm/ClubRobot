9a68558da15cd59eed7ad135e9c0195211967bc8
fayce <gfayce@gmail.com>
2025-09-19 13:22:02 +0100

    bidi

--- microbit/07-Radio/3.bidirectionnel.py
+++ microbit/07-Radio/3.bidirectionnel.py
@@ -4,7 +4,14 @@ import radio
 radio.config(group=8)
 radio.on()
 
+message = ''
+
 while True:
+    message = radio.receive()
+
+    if message:
+        display.scroll(message)
+        message = ''
 
     if button_a.is_pressed():
         sleep(200)
@@ -17,5 +24,3 @@ while True:
     if pin_logo.is_touched():
         sleep(200)
         radio.send('Quoi?')
-
-
